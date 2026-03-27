fa4aa7e5310320fcf879fbaa1780fdd4b3a550de
youngtaekiim <youngtae88.kim@lge.com>
2026-03-26 22:08:57 -0400

    add led pattern in passive buzzer

--- nuc_base/nuc_guest/arduino/ardn_buzzer_passive/ardn_buzzer_passive.ino
+++ nuc_base/nuc_guest/arduino/ardn_buzzer_passive/ardn_buzzer_passive.ino
@@ -3,14 +3,40 @@
 // Board : Arduino Uno 
 // By : Kit Plus
 //*******************************************************************************
+#include <Adafruit_NeoPixel.h>
 
-int DigitalPin = 7;  // Digital input
+#define BUZZER_PIN 7
+#define LED_PIN 6
+#define N_LEDS 8
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+const uint32_t RED = Adafruit_NeoPixel::Color(255, 0, 0);
+const uint32_t OFF = 0;
 
 void setup()
 {
-    pinMode(DigitalPin, OUTPUT);
+    strip.setBrightness(100);
+    strip.begin();
+    showAll(OFF);
+
+    pinMode(BUZZER_PIN, OUTPUT);
     Serial.begin(115200);
     while (!Serial) { }
+}
+
+void showAll(uint32_t c) {
+    for (uint16_t i = 0; i < strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+    }
+    strip.show();
+}
+
+void showHalf(uint32_t c) {
+    for (uint16_t i = 0; i < strip.numPixels() / 2; i++) {
+        strip.setPixelColor(i, c);
+    }
+    strip.show();
 }
 
 void loop()
@@ -18,9 +44,11 @@ void loop()
     if (Serial.available() > 0) {
         char c = Serial.read();
         if (c == '1') {
-            tone(DigitalPin, 1000); // 1000Hz ON
+            tone(BUZZER_PIN, 1000); // 1000Hz ON
+            showHalf(RED);
         } else if (c == '0') {
-            noTone(DigitalPin);     // OFF
+            noTone(BUZZER_PIN);     // OFF
+            showAll(OFF);
         }
     }
 
